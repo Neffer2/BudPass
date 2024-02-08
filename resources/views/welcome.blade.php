93cdb1c9f5f94ceb3d034e5a76fe85c13bc04a6f
Sebastian Camargo <juansecapri@hotmail.com>
2024-02-08 11:35:55 -0500

    Form registro movil avances

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,25 @@
 @section('content')
     <div class="forms-container">
 
-        <div class="form-btn">
-            <button id="show-login-btn" class="regs-btn ">Login</button>
-            <button id="show-register-btn" class="regs-btn inactive">Registro</button>
-        </div>
+        <div class="main-forms-container">
 
-        <div class="regs-container">
-
-            <div id="login-component">
-                <livewire:auth.login />
+            <div class="main-forms-img">
+            </div>
+            <div class="form-btn">
+                <button id="show-login-btn" class="regs-btn ">Login</button>
+                <button id="show-register-btn" class="regs-btn inactive">Registro</button>
             </div>
 
-            <div id="register-component" style="display: none;">
-                <livewire:auth.register />
+            <div class="regs-container">
+
+                <div id="login-component">
+                    <livewire:auth.login />
+                </div>
+
+                <div id="register-component" style="display: none;">
+                    <livewire:auth.register />
+                </div>
             </div>
         </div>
     </div>
-
 @endsection
