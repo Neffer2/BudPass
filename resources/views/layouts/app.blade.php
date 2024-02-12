22c2d1be358aec5c64d4c2274d96030b542c273f
Sebastian Camargo <juansecapri@hotmail.com>
2024-02-12 14:23:16 -0500

    Se creó un nuevo archivo para estilos de login y registro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login-registro.css') }}">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <title>@yield('title')</title>
 </head>
 
 <body>
+    @auth
     <header>
         <div class="header-content">
             <div class="header-img">
@@ -57,6 +59,7 @@
             </div>
         </div>
     </header>
+    @endauth
 
     <nav>
         @auth
