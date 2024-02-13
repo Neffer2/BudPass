0b442e0525b1a9f21e80d5e5af620e45689bd105
Sebastian <juansecapri@hotmail.com>
2024-02-12 20:21:42 -0500

    Avances login

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,23 +1,28 @@
 <div class="login-div-cont">
-    <form action="/login" method="POST">
+    <div class="login-title-text">Iniciar sesión</div>
+    <form action="/login" class="login-form" method="POST">
         @csrf
-        <div>
-            <label for="email_login">Nombre: </label>
+        <div class="input-login-form">
+            <label for="email_login">Email: </label>
             <input id="email_login" type="email" name="email">
         </div>
-        <div>
-            <label for="password_login"></label>
+        <div class="input-login-form">
+            <label for="password_login">Contraseña: </label>
             <input id="password_login" type="password" name="password">
-        </div>
-        <div>
-            <button type="submit">Iniciar</button>
         </div>
         @if ($errors->any())
             @foreach ($errors->all() as $error)
-                <div class="text-invalid">
+                <div class="text-invalid-login">
                     {{ $error }}
                 </div>
             @endforeach
         @endif
+        <div>
+            <button type="submit">Iniciar</button>
+        </div>
+        <div>
+            <a href="">Recuperar Contraseña</a>
+        </div>
+
     </form>
 </div>
