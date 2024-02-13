63c2e53351aaa369a2f98689b2031fc71238ad0a
Sebastian Camargo <juansecapri@hotmail.com>
2024-02-13 09:42:51 -0500

    Imagen temporal en login-registro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,26 +3,28 @@
     <div class="page-forms-cont">
 
         <div class="main-forms-cont">
-                <div class="main-forms-logo">
-                    <div class="big-logo-cont"></div>
-                </div>    
-                <div class="regs-form-cont">
-                    <div class="form-btn">
-                        <button id="show-login-btn" class="regs-btn ">Iniciar sesión</button>
-                        <button id="show-register-btn" class="regs-btn secundario-btn">Registro</button>
-                    </div>
-                    <div id="login-component">
-                        <livewire:auth.login />
-                    </div>
-                    <div id="register-component" style="display: none;">
-                        <livewire:auth.register />
-                    </div>
+            <div class="main-forms-logo">
+                <div class="big-logo-cont">
+                    <img src="{{ asset('assets/budweiser/logo-budweiser.svg') }}" alt="Logo Budweiser" class=""
+                        srcset="">
                 </div>
+            </div>
+            <div class="regs-form-cont">
+                <div class="form-btn">
+                    <button id="show-login-btn" class="regs-btn ">Iniciar sesión</button>
+                    <button id="show-register-btn" class="regs-btn secundario-btn">Registro</button>
+                </div>
+                <div id="login-component">
+                    <livewire:auth.login />
+                </div>
+                <div id="register-component" style="display: none;">
+                    <livewire:auth.register />
+                </div>
+            </div>
         </div>
     </div>
 
     <script>
-
         // TODO: Hacer una sección en el layout para poner todos los scripts
         const togglePasswordVisibility = (id) => {
             const passwordInput = document.getElementById(id);
