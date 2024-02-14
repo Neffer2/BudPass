4eb8e43a1a2ab15a2ef4a0f8cd9a4d021357cc38
Sebastian <juansecapri@hotmail.com>
2024-02-13 23:39:58 -0500

    Avances en registro factura desktop

--- resources/views/dashboard/registro.blade.php
+++ resources/views/dashboard/registro.blade.php
@@ -1,28 +1,41 @@
 @extends('layouts.app')
 @section('content')
-    <div class="main-facturas-cont">
-        <div class="big-logo-facturas-cont">
-            <div class="big-logo-facturas"></div> {{-- cambiar --}}
-        </div>
-        <div class="facturas-codigos-cont">
+    <div class="cont-main-registro-facturas-codigos">
+        <div class="main-facturas-cont">
+            <div class="big-logo-facturas-cont">
+                <div class="big-logo-facturas"></div>
+                <div class="big-logo-facturas-desk"></div>
+                <div class="big-logo-facturas-desk"></div>
+            </div>
+            <div class="facturas-codigos-cont">
 
-            <div class="form-facturas-btn">
-                <button id="show-facturas-btn" class="facturas-codigos-btn">Registrar facturas</button>
-                <button id="show-codigo-btn" class="facturas-codigos-btn secundario-btn">Registrar códigos</button>
+                <div class="form-facturas-btn">
+                    <button id="show-facturas-btn" class="facturas-codigos-btn">Registrar facturas</button>
+                    <button id="show-codigo-btn" class="facturas-codigos-btn secundario-btn">Registrar códigos</button>
+                </div>
+                <div class="registro-factura-form">
+                    <livewire:dashboard.registro-facturas />
+                </div>
+                <div class="registro-codigo-form" style="display: none;">
+                    <livewire:dashboard.registro-codigo />
+                </div>
+
             </div>
-            <div class="registro-factura-form">
-                <livewire:dashboard.registro-facturas />
-            </div>
-            <div class="registro-codigo-form" style="display: none;">
-                <livewire:dashboard.registro-codigo />
-            </div>
+        </div>
+        <div class="info-puntaje-cont">
             <div class="puntaje-ac">
-                PUNTAJE ACUMULADO
-                {{ Auth::user()->puntos }} PTS <br>
-                PUNTAJE PENDIENTE
-                {{ Auth::user()->puntos }}
+                <div class="puntaje-ac-text">
+                    <p class="text-puntaje-ac">PUNTAJE ACUMULADO <span>{{ Auth::user()->puntos }} PTS</span></p>
+                    <p class="text-puntaje-pen">PUNTAJE PENDIENTE <span>{{ Auth::user()->puntos }} PTS</span></p>
+                </div>
+            </div>
+            <div class="items-factura-contaner"><!-- Nombre puede cambiar -->
+                <div class="items-img-factura-cont">
+                    @for ($i = 0; $i < 9; $i++)
+                        <div class="items-img-factura"></div>
+                    @endfor
+                </div>
             </div>
         </div>
     </div>
-
 @endsection
