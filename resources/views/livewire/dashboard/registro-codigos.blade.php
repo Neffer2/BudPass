ba4eae29643c2cfe1e035179202669fbd6db963f
Sebastian Camargo <juansecapri@hotmail.com>
2024-02-14 12:18:42 -0500

    Se arreglaron margins y se agregó boostrap

--- resources/views/livewire/dashboard/registro-codigos.blade.php
+++ resources/views/livewire/dashboard/registro-codigos.blade.php
@@ -1,23 +1,26 @@
-<div>
-    <div>
-        <label for="">CODIGO</label>
-        <input type="text" wire:model="codigo">
+<div class="registro-codigo-form">
+    <div class="codigo-cont">
+        <div class="codigo-text">
+            <label for="">Codigos: </label>
+            <input type="text" wire:model="codigo">
+            @error('codigo')
+                <div class="text-invalid-codigo">
+                    {{ $message }}
+                </div>
+            @enderror
+            @error('codigo-bloqueado')
+                <div class="text-invalid-codigo">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
     </div>
-    <div>
+    <div class="codigo-btn-cont">
         <button wire:click="storePuntos" @error('codigo-bloqueado') disabled @enderror wire:loading.attr="disabled"
             wire:target="storePuntos">Canjear</button>
     </div>
-    @error('codigo')
-        <div class="text-invalid-codigo">
-            {{ $message }}
-        </div>
-    @enderror
-    @error('codigo-bloqueado')
-        <div class="text-invalid-codigo">
-            {{ $message }}
-        </div>
-    @enderror
     @if (session('success-registro-codigo'))
         <b>{{ session('success-registro-codigo') }}</b>
     @endif
 </div>
+
