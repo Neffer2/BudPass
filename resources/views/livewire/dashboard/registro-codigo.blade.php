0a4ff8e0f17d35aed232042060c13f8a883cd86c
Sebastian Camargo <juansecapri@hotmail.com>
2024-02-14 11:05:55 -0500

    Formulario codigo movil y desktop

--- resources/views/livewire/dashboard/registro-codigo.blade.php
+++ resources/views/livewire/dashboard/registro-codigo.blade.php
@@ -1,23 +1,25 @@
-<div>
-    <div>
-        <label for="">CODIGO</label>
+<div class="codigo-cont">
+    <div class="codigo-text">
+        <label for="">Codigo: </label>
         <input type="text" wire:model="codigo">
+        @error('codigo')
+            <div class="text-invalid-codigo">
+                {{ $message }}
+            </div>
+        @enderror
+        @error('codigo-bloqueado')
+            <div class="text-invalid-codigo">
+                {{ $message }}
+            </div>
+        @enderror
     </div>
-    <div>
+    <div class="codigo-btn-cont">
         <button wire:click="storePuntos" @error('codigo-bloqueado') disabled @enderror wire:loading.attr="disabled"
             wire:target="storePuntos">Canjear</button>
     </div>
-    @error('codigo')
-        <div class="text-invalid-codigo">
-            {{ $message }}
-        </div>
-    @enderror
-    @error('codigo-bloqueado')
-        <div class="text-invalid-codigo">
-            {{ $message }}
-        </div>
-    @enderror
     @if (session('success-registro-codigo'))
         <b>{{ session('success-registro-codigo') }}</b>
     @endif
+
+
 </div>
