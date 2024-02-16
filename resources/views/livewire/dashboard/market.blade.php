7924cd6b40e115e67de0da18ef62b635ebb1002c
Sebastian Camargo <juansecapri@hotmail.com>
2024-02-16 15:02:00 -0500

    Avances marketplace

--- resources/views/livewire/dashboard/market.blade.php
+++ resources/views/livewire/dashboard/market.blade.php
@@ -9,22 +9,36 @@
 
         <div class="premios-cont">
             <br>
-            @foreach ($premios as $premio)
-                <div @class(['disabled' => $puntosUser < $premio->puntos])>
-                    <h3>{{ $premio->nombre }}</h3>
-                    <p>{{ $premio->descripcion }} - Puntos: {{ $premio->puntos }}</p>
-
-                    <img src='{{ asset("assets/premios/$premio->foto") }}' height="50" alt="">
-                    <button
-                        @if ($puntosUser >= $premio->puntos) class="disabled" x-on:click="$wire.redimir({{ $premio->id }})" @endif>
-                        @if ($puntosUser >= $premio->puntos)
-                            Redimir
-                        @else
-                            No disponible
-                        @endif
-                    </button>
+            <div class="carousel-movil">
+                @foreach ($premios->chunk(2) as $chunk)
+                    <div class="carousel-page-movil">
+                        @foreach ($chunk as $premio)
+                            <div @class([
+                                'premios-img-cont' => true,
+                                'disabled-premio' => $puntosUser < $premio->puntos,
+                            ])>
+                                {{-- <h3>{{ $premio->nombre }}</h3> --}}
+                                {{-- <p>{{ $premio->descripcion }} - Puntos: {{ $premio->puntos }}</p> --}}
+                                <img src='{{ asset("assets/premios/$premio->foto") }}' height="50" alt="">
+                                {{-- <button
+                                    @if ($puntosUser >= $premio->puntos) class="disabled" x-on:click="$wire.redimir({{ $premio->id }})" @endif>
+                                    @if ($puntosUser >= $premio->puntos)
+                                        Redimir
+                                    @else
+                                        No disponible
+                                    @endif
+                                </button> --}}
+                            </div>
+                        @endforeach
+                    </div>
+                @endforeach
+                <div class="carusel-btn-movil">
+                    <button id="prev-carusel-movil">Prev</button>
+                    <button id="next-carusel-movil">Next</button>
                 </div>
-            @endforeach
+
+            </div>
+
         </div>
         <div class="redenciones-premios-cont" style="background-color: aqua;">
             <h2>Tus redenciones</h2>
@@ -64,3 +78,26 @@
         {{ session('success') }}
     @endif
 </div>
+
+<script>
+    let index = 0;
+    const pages = document.querySelectorAll('.carousel-page-movil');
+
+    function showPage(index) {
+        pages.forEach((page, i) => {
+            page.style.display = i === index ? 'flex' : 'none';
+        });
+    }
+
+    document.getElementById('prev-carusel-movil').addEventListener('click', () => {
+        index = Math.max(0, index - 1);
+        showPage(index);
+    });
+
+    document.getElementById('next-carusel-movil').addEventListener('click', () => {
+        index = Math.min(pages.length - 1, index + 1);
+        showPage(index);
+    });
+
+    showPage(index);
+</script>
