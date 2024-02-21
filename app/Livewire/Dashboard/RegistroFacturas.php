73da63703da582ff2bc56ce9e79ffbcd1df83855
Neffer <n3f73r@gmail.com>
2024-02-20 21:50:07 -0500

    Avances POSTBACK

--- app/Livewire/Dashboard/RegistroFacturas.php
+++ app/Livewire/Dashboard/RegistroFacturas.php
@@ -181,6 +181,84 @@ class RegistroFacturas extends Component
         }
     }
 
+    // private function __sendTD($form_data, $country, $brand, $campaign, $form, $unify, $production){
+    //     $td_env = $production ? 'prod' : 'dev';
+    //     $http_protocol = isset($_SERVER['https']) ? 'https://' : 'http://';
+            
+    //     $form_data['abi_brand'] = $brand;
+    //     $form_data['abi_campaign'] = $campaign;
+    //     $form_data['abi_form'] = $form;
+    //     $form_data['td_unify'] = $unify;
+    //     $form_data['td_import_method'] = 'postback-api-1.2';
+    //     $form_data['td_client_id'] = $_COOKIE['_td'];
+    //     $form_data['td_url'] = $http_protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    //     $form_data['td_host'] = $_SERVER['HTTP_HOST'];
+            
+    //     $td_country = $country;
+    //     $td_apikey = $td_env !== 'prod' ? '9648/41e45454b77308046627548e0b4fe2ddbc0893d2' : '10086/9c06ed6fa48e0fb6952ed42773cca1cc1d43684e';
+            
+    //     $country_zone_mapping = array("nga"=>"africa", "zwe"=>"africa", "zaf"=>"africa", "aus"=>"apac", "chn"=>"apac", "ind"=>"apac", "jpn"=>"apac", "kor"=>"apac", "tha"=>"apac", "vnm"=>"apac", "bel"=>"eur", "fra"=>"eur", "deu"=>"eur", "ita"=>"eur", "nld"=>"eur", "rus"=>"eur", "esp"=>"eur", "ukr"=>"eur", "gbr"=>"eur", "col"=>"midam", "dom"=>"midam", "ecu"=>"midam", "slv"=>"midam", "gtm"=>"midam", "hnd"=>"midam", "mex"=>"midam", "pan"=>"midam", "per"=>"midam", "can"=>"naz", "usa"=>"naz", "arg"=>"saz", "bol"=>"saz", "bra"=>"saz", "chl"=>"saz", "ury"=>"saz");
+            
+    //     $td_zone = $country_zone_mapping[$td_country];
+    //     $curl = curl_init();
+        
+    //     $curl_opts = array(
+    //         CURLOPT_URL => "https://in.treasuredata.com/postback/v3/event/{$td_zone}_source/{$td_country}_web_form",
+    //         CURLOPT_POST => true,
+    //         CURLOPT_RETURNTRANSFER => true,
+    //         CURLOPT_HTTPHEADER => array(
+    //             'Content-Type: application/json',
+    //             "X-TD-Write-Key: {$td_apikey}"
+    //         ),
+    //         CURLOPT_POSTFIELDS => json_encode($form_data)
+    //     );
+        
+    //     curl_setopt_array($curl, $curl_opts);
+        
+    //     $response = @curl_exec($curl);
+    //     $response_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        
+    //     curl_close($curl);
+    //     return $response_code;
+    // }
+       
+    /*
+        // define variable that will be used to tell the __sendTD method if it should send to the production database
+        $is_production = false;
+        
+            // define the purpose variable as an empty array
+        $purposes = array();
+        
+            // check whether the TC-PP checkbox is checked, and if it is, then adds it to the purpose array - informed
+        if($tcpp) $purposes[] = 'TC-PP';
+        
+            // check whether the MARKETING-ACTIVATION checkbox is checked, and if it is, then adds it to the purpose array
+        if($marketing) $purposes[] = 'MARKETING-ACTIVATION';
+        
+            // here it's possible to add additional purposes to the purpose array
+        
+            // runs the __sendTD method with parameters got from the request, it should be changed based on your form fields, country, brand, campaign, form, and whether if it's running in the production environment or not
+        $tdstatus = $this->__sendTD(
+                array(
+                "abi_name" => $name,
+                "abi_cpf" => $cpf,
+                "abi_email" => $email,
+                "abi_city" => $city,
+                "abi_dayofbirth" => $dayofbirth,
+                "abi_monthofbirth" => $monthofbirth,
+                "abi_yearofbirth" => $yearofbirth,
+                "abpurposei_interests" => '',
+            "_name" => $purposes,
+                ), // form data & purposes
+            'col', // country
+            'Budweiser', // brand
+            "BUDWEISER_BUDPASS_02_24", // campaign
+            "BUDWEISER_BUDPASS_02_24", // form
+            false, // unify
+            $is_production // production flag
+        );
+    */
+
     // VALIDATIONS
     public function messages() 
     {
