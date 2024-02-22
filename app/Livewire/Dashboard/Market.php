383ed1008210a888fc528383b0d0c22c18bc3503
Neffer <n3f73r@gmail.com>
2024-02-22 18:52:14 -0500

    1. Registro en tabla bbdd_registro_budpass funcional

--- app/Livewire/Dashboard/Market.php
+++ app/Livewire/Dashboard/Market.php
@@ -53,6 +53,38 @@ class Market extends Component
         $modelPremio->stock -= 1;
         $modelPremio->update();
 
+
+        $registros_factura = $this->user->registrosFactura->map(function ($registro){
+            return $registro->num_factura;
+        });
+
+        $numeros_nit = $this->user->registrosFactura->map(function ($registro){
+            return $registro->canal->nit;
+        });
+
+        $codigos_redimidos = $this->user->registrosCodigo->map(function ($registro){
+            return $registro->codigo->codigo;
+        });
+
+        $redenciones = $this->user->redenciones->map(function ($redencion){
+            return $redencion->premio->nombre;
+        });
+
+        // Store BUD TABLE
+        $registro = new bbdd_registro_budpass();
+        $registro->nombre = $this->user->name;
+        $registro->correo = $this->user->email;
+        $registro->fecha_nacimiento = $this->user->fecha_nacimiento;
+        $registro->ciudad = $this->user->ciudad->descripcion;
+        $registro->numero_factura = $registros_factura->toJson();
+        $registro->numero_nit = $numeros_nit->toJson();
+        $registro->codigos_redimidos = $codigos_redimidos->toJson();
+        $registro->puntaje_acumulado = $this->user->puntos;
+        $registro->premios_redimidos = $redenciones->toJson();
+        $registro->puesto_ranking = $this->user->rank($this->user->id);
+        $registro->save();
+        // 
+
         return redirect()->route('market')->with([
             'title' => 'Redención exitosa.',
             'success-redencion' => '¡Felicidades! haz redimido el premio: '.$this->premios->where('id', $premio)->first()->nombre.', pronto nos contactaremos contigo.'
