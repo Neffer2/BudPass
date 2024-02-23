bc6ba5886fbfa0b525dcab98ff5584b51f18864a
Sebastian Camargo <juansecapri@hotmail.com>
2024-02-23 18:04:18 -0500

    Cambios en mailing

--- resources/views/mail/main.blade.php
+++ resources/views/mail/main.blade.php
@@ -17,10 +17,10 @@
                     style="font-size: 14px; line-height: 1.5; height: 100%; margin: 0; padding: 40px; text-align: center; background-color: #f0f0f0; color: #0a0541;">
                     <h1
                         style="font-size: 28px; font-family: 'Elephant', serif; font-style: normal; font-weight: 300; text-transform: uppercase; margin: 0; padding: 0;">
-                        {{ $title }}</h1>
+                        {!! $title !!}</h1>
                     <p
                         style="font-size: 16px; font-family: Helvetica, Arial, sans-serif; margin: 0; margin-top: 20px; padding: 0;">
-                        {{ $body }}
+                        {!! $body !!}
                     </p>
                 </div>
                 <a href="http://budpass.co" target="_blank" rel="noopener noreferrer"><img
