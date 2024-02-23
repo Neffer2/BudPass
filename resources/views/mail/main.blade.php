d0d98687a85acdba8d0b9286bd453333b7b1c5bc
Sebastian Camargo <juansecapri@hotmail.com>
2024-02-23 16:17:25 -0500

    Mailing listo para revisión

--- resources/views/mail/main.blade.php
+++ resources/views/mail/main.blade.php
@@ -1,16 +1,36 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body style="font-family: Arial, sans-serif; color: #333; text-align: center;">
-    <img src="https://dev.budpass.co/assets/mailing/mailing-header.jpg" alt="Mailing Header" style="width: 50%; height: auto; display: block; margin: auto;">
-    <p style="font-size: 16px; line-height: 1.5; margin: 20px 0;">
-        {{ $body }}
-    </p>
-    <img src="https://dev.budpass.co/assets/mailing/mailing-body.jpg" alt="Mailing Body" style="width: 50%; height: auto; display: block; margin: auto;">
-    <img src="https://dev.budpass.co/assets/mailing/mailing-footer.jpg" alt="Mailing Footer" style="width: 50%; height: auto; display: block; margin: auto;">
+    <table style="max-width: 600px; width: 100%; margin: auto;">
+        <tr>
+            <td>
+                <img style="width: 100%; height: auto; display: block; margin: 0; padding: 0;"
+                    src="https://dev.budpass.co/assets/mailing/mailing-header.jpg" alt="Mailing Header">
+                <div
+                    style="font-size: 14px; line-height: 1.5; height: 100%; margin: 0; padding: 40px; text-align: center; background-color: #f0f0f0; color: #0a0541;">
+                    <h1
+                        style="font-size: 28px; font-family: Georgia, 'Times New Roman', Times, serif; font-style: italic; margin: 0; padding: 0;">
+                        {{ $title }}</h1>
+                    <p
+                        style="font-size: 16px; font-family: Helvetica, Arial, sans-serif; margin: 0; margin-top: 20px; padding: 0;">
+                        {{ $body }}
+                    </p>
+                </div>
+                <a href="http://budpass.co" target="_blank" rel="noopener noreferrer"><img
+                        style="width: 100%; height: auto; display: block; margin: 0; padding: 0;"
+                        src="https://dev.budpass.co/assets/mailing/mailing-body.jpg" alt="Mailing Body"></a>
+                <img style="width: 100%; height: auto; display: block; margin: 0; padding: 0;"
+                    src="https://dev.budpass.co/assets/mailing/mailing-footer.jpg" alt="Mailing Footer">
+            </td>
+        </tr>
+    </table>
 </body>
-</html> 
+
+</html>
