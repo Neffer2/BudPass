70c0b658f3ab23d13bcc9be69c69505c283468ff
Sebastian Camargo <juansecapri@hotmail.com>
2024-02-23 15:09:17 -0500

    Avances mailing

--- resources/views/mail/main.blade.php
+++ resources/views/mail/main.blade.php
@@ -5,10 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body>
-    <img src="{{ asset('assets/mailing/mailing-header.jpg') }}" alt="Mailing Header">
-    {{ $body }}
-    <img src="{{ asset('assets/mailing/mailing-body.jpg') }}" alt="Mailing Body">
-    <img src="{{ asset('assets/mailing/mailing-footer.jpg') }}" alt="Mailing Footer">
+<body style="font-family: Arial, sans-serif; color: #333; text-align: center;">
+    <img src="https://dev.budpass.co/assets/mailing/mailing-header.jpg" alt="Mailing Header" style="width: 50%; height: auto; display: block; margin: auto;">
+    <p style="font-size: 16px; line-height: 1.5; margin: 20px 0;">
+        {{ $body }}
+    </p>
+    <img src="https://dev.budpass.co/assets/mailing/mailing-body.jpg" alt="Mailing Body" style="width: 50%; height: auto; display: block; margin: auto;">
+    <img src="https://dev.budpass.co/assets/mailing/mailing-footer.jpg" alt="Mailing Footer" style="width: 50%; height: auto; display: block; margin: auto;">
 </body>
 </html> 
