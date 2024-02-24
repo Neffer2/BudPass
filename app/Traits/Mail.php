2373ee1139dd328bb32c9dcf3df0749f69368a79
Neffer <n3f73r@gmail.com>
2024-02-23 20:09:47 -0500

    Correciones copy mailing

--- app/Traits/Mail.php
+++ app/Traits/Mail.php
@@ -11,23 +11,13 @@ trait Mail
         $subject = "¡Celebramos tu grandeza, bienvenido a Budpass!";
         $content = [
             'view' => 'mail.main',
-            'title' => "Budweiser transforma tus compras en momentos inolvidables. ¡Registra y redime tus premios ahora!",
+            'title' => "<h1>¡REGISTRO EXITOSO! Sube tus facturas y gana boletas al FEP y más.</h1>",
             'body' => "<p>
-                ¡Bienvenido {$user->name}!<br>
-                Budweiser premia tu lealtad con experiencias únicas y grandes premios.<br>
-                ¡Empieza a registrar tus facturas y participa ahora por excelentes recompensas!<br>
-                <strong>Premios Exclusivos:</strong><br>
-                Mercancia oficial de Budweiser hasta experiencias VIP, tenemos premios que te harán sentir la grandeza en cada detalle.<br>
-                <strong>Experiencias Inolvidables:</strong><br>
-                Con el ranking de premios vive momentos únicos que solo Budweiser puede ofrecerte.<br>
-                No pierdas la oportunidad de ser parte de esta experiencia única. Registra tus facturas ahora. ¡Los premios te están esperando!<br>
-                <a href='https://www.budpass.co/'>Haz clic aquí para empezar</a><br>
-                ¡Celebremos juntos la grandeza con Budweiser!<br>
-                Equipo Budweiser<br>
-                P.S.: Recuerda, la grandeza está al alcance de tu mano. Registra tus facturas y prepárate para ganar grandes premios con Budweiser.
+                <h2>¡Hola {$user->name}!</h2><br>
+                Ya estás registrado, ahora acumula puntos con tus compras de Budweiser para redimirlos por entradas, transporte y mucho más.
             </p>"
         ];
-        $altBody = "Esta es mi canción de bienvenida"; 
+        $altBody = "¡REGISTRO EXITOSO! Sube tus facturas y gana boletas al FEP y más!"; 
 
         $this->sendMail($subject, $content, $altBody, $user);
     } 
@@ -38,22 +28,10 @@ trait Mail
             $subject = "¡Budweiser te informa! ¡Tus puntos se registraron con éxito!";
             $content = [
                 'view' => 'mail.main',
-                'title' => "¡Felicidades! Tus puntos Budweiser han sido registrados con éxito. ¡Descúbrelos ahora!",
+                'title' => "<h1>¡Tus puntos han sido acumulados!</h1>",
                 'body' => "<p>
-                    ¡Hola {$regsitro->shopper->name}!<br>
-                    Nos alegra saludarte de nuevo<br>
-                    En Budweiser, reconocemos y celebramos la grandeza en cada uno de nuestros seguidores. Es por eso por lo que estamos emocionados de informarte que tu registro de puntos ha sido exitoso.<br>
-                    Ahora, es momento de que disfrutes de todas las recompensas que te mereces.<br>
-                    Por tu compra en {$regsitro->canal->descripcion}<br>
-                    Acumulaste {$regsitro->puntos_sumados} puntos.<br>
-                    Tu nos inspiras a ofrecerte experiencias únicas y premios exclusivos que reflejen la grandeza que compartimos.<br>
-                    ¡Sigue acumulando puntos con cada compra y descubre todo lo que Budweiser tiene para ti!<br>
-                    No pierdas más tiempo y comienza a disfrutar de tus beneficios como miembro de nuestra familia Budweiser.<br>
-                    ¡Registra el resto de tus puntos hoy mismo y descubre la grandeza que te espera!<br>
-                    Haz clic <a href='https://www.budpass.co/'>aquí</a> para empezar:<br>
-                    ¡Celebremos juntos la grandeza con Budweiser!<br>
-                    Equipo Budweiser<br>
-                    P.S.: Recuerda, la grandeza está al alcance de tu mano. Registra tus facturas y prepárate para ganar grandes premios con Budweiser.
+                    <h2>¡Hola {$regsitro->shopper->name}!<br></h2>
+                    Acabas de acumular puntos, continua acumulando por el Ranking por las entradas para el FEP2024. fila rápida, transporte o redimelos por muchos más.
                 </p>"    
             ];
             $altBody = "Registro de Factura Validado, haz ganado {$regsitro->puntos_sumados} puntos en el canal {$regsitro->canal->descripcion}."; 
@@ -61,19 +39,8 @@ trait Mail
             $subject = "¡Ups, tu factura fue rechazada!";
             $content = [
                 'view' => 'mail.main',
-                'title' => "No pierdas tu grandeza, en Budweiser creemos en las segundas oportunidades",
-                'body' => "<p>
-                    ¡{$regsitro->shopper->name}, no te preocupes!<br>
-                    En Budweiser recompensamos tu lealtad y queremos que redimas tus premios.<br>
-                    Registra nuevamente tu factura y ten presente la siguiente información:<br>
-                    1. Revisa que los datos que ingresaste en la plataforma coincidan con la factura.<br>
-                    2. Que la fotografía que cargaste sea legible<br>
-                    3. Que tu factura no se encuentre registrada en la DIAN<br>
-                    Haz clic <a href='https://www.budpass.co/'>aquí</a> para realizar tu registro nuevamente<br>
-                    ¡Celebremos juntos la grandeza con Budweiser!<br>
-                    Equipo Budweiser<br>
-                    P.S.: Recuerda, la grandeza está al alcance de tu mano. Registra tus facturas y prepárate para ganar grandes premios con Budweiser.
-                </p>"
+                'title' => "h1>¡Lo sentimos! tu factura no es valida.</h1>",
+                'body' => "<p> Por favor toma una foto legible y aplicable de acuerdo a los términos y condiciones y vuelve a cargarla para acumular tus puntos.</p>"
             ]; 
             $altBody = "Registro de Factura Validado."; 
         }
@@ -85,18 +52,10 @@ trait Mail
         $subject = "¡Felicidades por tu logro! ¡Disfruta tu premio Budweiser!";
         $content = [
             'view' => 'mail.main',
-            'title' => "¡Felicidades! Tus puntos Budweiser han sido registrados con éxito. ¡Descúbrelos ahora!",
+            'title' => "<h1>¡Redención exitosa!</h1>",
             'body' => "<p>
-                ¡Felicitaciones {$user->name}!<br>
-                Es un honor para nosotros formar parte de tu grandeza y recompensar tu lealtad con experiencias únicas.<br>
-                ¡El premio que has seleccionado es {$premio->nombre}!<br>
-                Tu dedicación y preferencia por Budweiser nos inspiran a seguir brindándote momentos extraordinarios. Queremos que disfrutes al máximo tu premio y que continúes celebrando la grandeza en cada sorbo de nuestras cervezas.<br>
-                No olvides que tú eres nuestra inspiración y nos impulsa a seguir ofreciéndote lo mejor.<br>
-                ¡Felicidades nuevamente por tu premio y gracias por ser parte de la familia Budweiser!<br>
-                ¡Celebremos juntos tu grandeza!<br>
-                Equipo Budweiser<br>
-                P.S.: Recuerda, la grandeza está al alcance de tu mano.<br>
-                <em>*Te llamaremos al número registrado, recuerda que realizaremos máximo tres intentos de contacto*</em>
+                <h2>¡Felicitaciones {$user->name}!</h2><br>
+                Redimiste tus puntos, muy pronto recibirás tu beneficio. En caso de ser físico el tiempo de entrega dependerá de tu ubicación, si es una entrada o un beneficio digital, muy pronto llegará a tu correo.
             </p>"    
         ];
         $altBody = "Premio redimido con éxito."; 
