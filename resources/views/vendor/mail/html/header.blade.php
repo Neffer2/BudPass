42c21e33cc0756a638099ec195fae82a6c8a7fa9
Neffer <n3f73r@gmail.com>
2024-02-26 09:28:59 -0500

    1. Change pass reset image

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,9 +3,9 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'BudPass')
-<img src="https://www.budpass.co/assets/budweiser/logo-budweiser.svg" class="logo" height="100" alt="Budweiser logo">
+<img src="{{ asset('assets/budweiser/logo-budweiser.svg') }}" class="logo" height="100" alt="Budweiser logo">
 @else
-<img src="https://www.budpass.co/assets/budweiser/logo-budweiser.svg" class="logo" height="100" alt="Budweiser logo">
+{{ $slot }}
 @endif
 </a>
 </td>
