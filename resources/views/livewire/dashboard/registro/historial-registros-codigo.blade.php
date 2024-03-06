d8339420ed60700819a836d14c763964966db334
Sebastian Camargo <juansecapri@hotmail.com>
2024-03-06 11:33:27 -0500

    Se agregó texto cuando no hay codigos o facturas registradas

--- resources/views/livewire/dashboard/registro/historial-registros-codigo.blade.php
+++ resources/views/livewire/dashboard/registro/historial-registros-codigo.blade.php
@@ -1,18 +1,22 @@
 <div class="codigos-historial-container">
     <h1>Códigos registrados</h1>
-    <table>
-        <thead>
-            <th>C&oacute;digo</th>
-            <th>Puntos</th>
-        </thead>
-        <tbody>
-            @foreach ($registrosCodigo as $registroCodigo)
-                <tr>
-                    <td>{{ $registroCodigo->codigo->codigo }}</td>
-                    <td>{{ $registroCodigo->puntos_sumados }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $registrosCodigo->links() }}
+    @if($registrosCodigo->isEmpty())
+        <p>No hay códigos registrados.</p>
+    @else
+        <table>
+            <thead>
+                <th>Código</th>
+                <th>Puntos</th>
+            </thead>
+            <tbody>
+                @foreach ($registrosCodigo as $registroCodigo)
+                    <tr>
+                        <td>{{ $registroCodigo->codigo->codigo }}</td>
+                        <td>{{ $registroCodigo->puntos_sumados }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{ $registrosCodigo->links() }}
+    @endif
 </div>
--- resources/views/livewire/dashboard/registro/historial-registros-factura.blade.php
+++ resources/views/livewire/dashboard/registro/historial-registros-factura.blade.php
@@ -1,26 +1,26 @@
 <div class="registros-historial-container">
     <h1>Facturas registradas</h1>
-    <table>
-        <thead>
-            <th>Factura</th>
-            <th>Canal</th>
-            <th>Puntos</th>
-            <th>Estado</th>
-        </thead>
-        <tbody>
-            @foreach ($registrosFactura as $registroFactura)
-                <tr>
-                    <td>{{ $registroFactura->num_factura }}</td>
-                    <td>{{ $registroFactura->canal->descripcion }}</td>
-                    <td>{{ $registroFactura->puntos_sumados }}</td>
-                    <td>{{ $registroFactura->estado_id }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $registrosFactura->links() }}
+    @if($registrosFactura->isEmpty())
+        <p>No hay facturas registradas.</p>
+    @else
+        <table>
+            <thead>
+                <th>Factura</th>
+                <th>Canal</th>
+                <th>Puntos</th>
+                <th>Estado</th>
+            </thead>
+            <tbody>
+                @foreach ($registrosFactura as $registroFactura)
+                    <tr>
+                        <td>{{ $registroFactura->num_factura }}</td>
+                        <td>{{ $registroFactura->canal->descripcion }}</td>
+                        <td>{{ $registroFactura->puntos_sumados }}</td>
+                        <td>{{ $registroFactura->estado_id }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{ $registrosFactura->links() }}
+    @endif
 </div>
-
-<script>
-    
-</script>
