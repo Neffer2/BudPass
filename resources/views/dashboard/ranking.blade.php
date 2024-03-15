172b4810e6e7e378bc56353bcce5e90f274d3ff7
Sebastian Camargo <juansecapri@hotmail.com>
2024-03-15 11:03:41 -0500

    Cambios en ranking final

--- resources/views/dashboard/ranking.blade.php
+++ resources/views/dashboard/ranking.blade.php
@@ -5,7 +5,7 @@
 
         <div class="ranking-table-cont">
             <div class="ranking-title">
-                <h1>Ranking</h1>
+                <h1>Ranking Final</h1>
             </div>
             <table>
                 <thead>
@@ -17,75 +17,22 @@
                     </tr>
                 </thead>
                 <tbody>
+                    @foreach ($ranking as $key => $participante)
                         <tr>
-                            <td>1</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td>5</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td>6</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td>7</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td>8</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td>9</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td>10</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                        </tr>
-
-                    @if (Auth::user()->estado_id == 1)
-                        <tr class="ranking-usuario-table">
+                            <td>{{ $key += 1 }}</td>
                             <td> - </td>
+                            <td> - </td>
+                            <td> - </td>
+                        </tr>
+                    @endforeach
+                    {{-- @if (Auth::user()->estado_id == 1)
+                        <tr class="ranking-usuario-table">
+                            <td>{{ $user_rank }}</td>
                             <td>{{ Auth::user()->name }}</td>
                             <td>{{ Auth::user()->puntos }}</td>
-                            <td>-</td>
+                            <td> - </td>
                         </tr>
-                    @endif
+                    @endif --}}
                 </tbody>
             </table>
         </div>
