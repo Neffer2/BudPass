199a441dd34f0ea5da0d9bf445fb446373c017aa
Sebastian Camargo <juansecapri@hotmail.com>
2024-03-15 14:50:41 -0500

    Se agregaron textos de no se encontraron redenciones.

--- resources/views/livewire/dashboard/market/redenciones.blade.php
+++ resources/views/livewire/dashboard/market/redenciones.blade.php
@@ -1,19 +1,23 @@
 <div class="container">
     <h2 class="text-center">Tus redenciones</h2>
-    <table>
-        <tr>
-            <th>Premio</td>
-            <th>Puntos</th>
-            <th>Fecha</th>
-        </tr>
-        @foreach ($redenciones as $redencion) 
+    @if ($redenciones->isEmpty())
+        <p class="text-center">No se encontraron redenciones.</p>
+    @else
+        <table>
             <tr>
-                <td>{{ $redencion->premio->nombre }}</td>
-                <td>{{ $redencion->premio->puntos }}</td>
-                <td>{{ $redencion->created_at }}</td>
+                <th>Premio</td>
+                <th>Puntos</th>
+                <th>Fecha</th>
             </tr>
-        @endforeach
-    </table>
+            @foreach ($redenciones as $redencion)
+                <tr>
+                    <td>{{ $redencion->premio->nombre }}</td>
+                    <td>{{ $redencion->premio->puntos }}</td>
+                    <td>{{ $redencion->created_at }}</td>
+                </tr>
+            @endforeach
+        </table>
+        {{ $redenciones->links() }}
+    @endif
 
-    {{ $redenciones->links() }}
 </div>
